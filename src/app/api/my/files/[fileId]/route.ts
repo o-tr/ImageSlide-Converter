d13abe54a101d7cca1744a1f7c9bf2fe33f79ebf
ootr <git@ootr.jp>
2024-08-22 03:13:45 +0900

    削除時にs3からも削除されるように修正

--- src/app/api/my/files/[fileId]/route.ts
+++ src/app/api/my/files/[fileId]/route.ts
@@ -2,6 +2,9 @@ import {auth} from "@/auth";
 import {getUser} from "@/lib/prisma/getUser";
 import {prisma} from "@/lib/prisma";
 import {NextResponse} from "next/server";
+import {s3NormalClient} from "@/lib/s3/normal";
+import {DeleteObjectCommand} from "@aws-sdk/client-s3";
+import {S3_NORMAL_BUCKET} from "@/const/env";
 
 export const DELETE = async(request:Request, { params: {fileId} }: { params: { fileId: string }},) => {
   const _auth = await auth();
@@ -12,10 +15,18 @@ export const DELETE = async(request:Request, { params: {fileId} }: { params: { f
   if (!user) return NextResponse.json({status: "error", error: "Unauthorized"});
   const file = user.files.find((file) => file.fileId === fileId);
   if (!file) return NextResponse.json({status: "error", error: "File not found"});
-  await prisma.file.delete({
-    where: {
-      id: file.id,
-    }
-  });
+  await Promise.all([
+    ...Array.from({length: file.count}, (_, index) => {
+      return s3NormalClient.send(new DeleteObjectCommand({
+        Bucket: S3_NORMAL_BUCKET,
+        Key: `${fileId}_${index}`,
+      }));
+    }),
+    prisma.file.delete({
+      where: {
+        id: file.id,
+      }
+    })
+  ])
   return NextResponse.json({status: "success"});
 }
