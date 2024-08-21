5e643f0430b41d41694fd429f38131a05d68cced
ootr <git@ootr.jp>
2024-08-21 20:37:28 +0900

    コピーロジックを実装

--- src/app/(_)/convert/completed/[fileId]/[size]/_components/URLDisplay.tsx
+++ src/app/(_)/convert/completed/[fileId]/[size]/_components/URLDisplay.tsx
@@ -1,6 +1,27 @@
-import {FC} from "react";
-import {Input} from "antd";
+"use client";
+import {FC, useState} from "react";
+import {Button, Input} from "antd";
+import Compact from "antd/es/space/Compact";
+import {TbCheck, TbCopy} from "react-icons/tb";
+
 
 export const URLDisplay:FC<{url: string}> = ({url}) => {
-  return <Input readOnly value={url}/>
+  const [isCopied, setIsCopied] = useState(false);
+  
+  const copy = () => {
+    navigator.clipboard.writeText(url).then(()=>{
+      setIsCopied(true);
+    });
+  }
+  
+  return(
+    <Compact block onClick={copy}>
+      <Input readOnly value={url}/>
+      {isCopied ? (
+        <Button type="primary" className={"w-32 bg-green-300"} icon={<TbCheck/>}>Copied!</Button>
+      ) : (
+        <Button type="primary" className={"w-32"} icon={<TbCopy />}>Copy</Button>
+      )}
+    </Compact>
+  )
 }
--- src/app/(_)/convert/completed/[fileId]/[size]/page.tsx
+++ src/app/(_)/convert/completed/[fileId]/[size]/page.tsx
@@ -1,6 +1,7 @@
 import {useMemo} from "react";
 import {URLDisplay} from "@/app/(_)/convert/completed/[fileId]/[size]/_components/URLDisplay";
 import {S3_NORMAL_PUBLIC_BASE_URL} from "@/const/env";
+import {Flex} from "antd";
 
 export default function Page({params}: {params: {fileId: string, size: string}}) {
   const urls = useMemo(()=>{
@@ -10,9 +11,11 @@ export default function Page({params}: {params: {fileId: string, size: string}})
     }
     return result;
   },[params]);
-  return <div>
-    {urls.map((url) => (
-      <URLDisplay url={url} key={url}/>
-    ))}
+  return <div className={"flex-1 grid place-items-center"}>
+    <Flex vertical gap={"middle"} className={"w-full lg:w-3/4"}>
+      {urls.map((url) => (
+        <URLDisplay url={url} key={url}/>
+      ))}
+    </Flex>
   </div>
 }
