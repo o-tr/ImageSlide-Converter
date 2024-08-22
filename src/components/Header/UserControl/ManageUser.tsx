0845afaedca8a6b21028d9d0294ca533d05fa0b4
ootr <git@ootr.jp>
2024-08-23 07:14:41 +0900

    ヘッダーを変更

--- src/components/Header/UserControl/ManageUser.tsx
+++ src/components/Header/UserControl/ManageUser.tsx
@@ -3,23 +3,12 @@ import {Button, Dropdown, MenuProps} from "antd";
 import {useSession,signOut} from "next-auth/react";
 import Image from "next/image";
 import {useRouter} from "next/navigation";
+import Link from "next/link";
 
 export const ManageUser = () => {
   const { data: session, status } = useSession()
-  const router = useRouter();
   
   const items: MenuProps['items'] = [
-    {
-      key: '0',
-      label: (
-        <span>
-          Files
-        </span>
-      ),
-      onClick: () => {
-        router.push("/my/files");
-      }
-    },
     {
       key: '1',
       label: (
@@ -35,8 +24,16 @@ export const ManageUser = () => {
   
   if (status !== "authenticated" || !session?.user) return null;
   return (
-    <Dropdown menu={{ items }} placement="bottomRight">
-      <Button className={"!p-[3px] !pr-[7px]"} icon={<Image src={session.user.image??""} alt={"icon"} width={24} height={24} className={"rounded-[2px]"}/>}>{session.user.name}</Button>
-    </Dropdown>
+    <>
+      <Link href={"/convert/pick"}>
+        <Button>Upload</Button>
+      </Link>
+      <Link href={"/my/files"}>
+        <Button>Files</Button>
+      </Link>
+      <Dropdown menu={{ items }} placement="bottomRight">
+        <Button className={"!p-[3px] !pr-[7px]"} icon={<Image src={session.user.image??""} alt={"icon"} width={24} height={24} className={"rounded-[2px]"}/>}>{session.user.name}</Button>
+      </Dropdown>
+    </>
   )
 }
