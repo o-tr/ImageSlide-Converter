fffb75696ad2abaed7b39ad1aea2bd31db8233b3
ootr <git@ootr.jp>
2024-08-23 06:26:50 +0900

    リンク切れとエラーの修正

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+"use client";
+import {Button, Result} from "antd";
+import Link from "next/link";
+import {AntContent} from "@/components/AntContent";
+
+export default function Page(){
+  return (
+    <AntContent className={"flex-1 grid place-items-center"}>
+      <Result
+        status="404"
+        title="404"
+        subTitle="ページは見つかりませんでした"
+        extra={<Link href={"/"}><Button type="primary">Back Home</Button></Link>}
+      />
+    </AntContent>
+  )
+}
